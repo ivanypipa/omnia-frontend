a38cfaf6dc9a16228bf253869009329138899af4
ivanypipa <ivanypipa@gmail.com>
2025-06-30 18:37:20 -0300

    🔀 Limpio App.jsx, elimino app.jsx, añado router de clientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
-const cliente = 'CajasNav'; // cambiá a 'B' para otro cliente
+// src/App.jsx
+import CajasNavApp from './clientes/CajasNav/App';
+import ConsultorioApp from './clientes/Consultorio/App';
+
+const cliente = 'CajasNav'; // o 'Consultorio'
+
+export default function App() {
+  if (cliente === 'CajasNav') return <CajasNavApp />;
+  if (cliente === 'Consultorio') return <ConsultorioApp />;
+  return <div className="flex items-center justify-center h-screen text-gray-500">
+    Cliente no autorizado
+  </div>;
+}
